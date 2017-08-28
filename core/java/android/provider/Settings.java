fc17f4e359594c95d753fff603a7ef08fa7b9f71
maxwen <max.weninger@gmail.com>
2017-08-27 18:37:00 -0700

    base: Global Actions with Advanced Reboot [1/2]
    With fixes from:
    
    Do not show reboot dialog in lock screen
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Dismiss dialog on screen off
    -Adin Kwok <adin.kwok@carbonrom.org>
    
    Vectorized the power menu sound panel selector
    -David Wilson <daveyannihilation@hotmail.com
    
    Fix silent mode icons in new power menu
    Fix airplane mode in new power menu
    -camcory <camcory1@gmail.com>
    
    Pimp out power menu
    -Alex Cruz <mazdarider23@gmail.com>
    
    Powermenu Torch option
    -LorDClockaN <davor@losinj.com>
    
    Add Screen Record to the Power Menu
    -eyosen <abittin@gmail.com>
    
    Power menu customizations [1/2]
    -Dave Kessler <activethrasher00@gmail.com>
    
    Replace restart to bootloader/recovery/system vectors.
    -Andrew Fluck <andrew@aospa.co>
    
    [anayw2001 edits: bring up to 11]
    
    [ghostrider-reborn edits]
    - bring up to 12
    - remove emergency button from reboot menu
    - use original restart icon in main menu
    - minor code cleanup
    
    Change-Id: I6978be623b0ec7ca9e702fc311449c36b504b1a6
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5516,6 +5516,11 @@ public final class Settings {
         public static final String DESKTOP_MODE = "desktop_mode";
 
 	/**
+         * @hide
+         */
+        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
+
+        /**
          * Whether to play notification sound and vibration if screen is ON
          * 0 - never
          * 1 - always
@@ -5540,6 +5545,11 @@ public final class Settings {
          * @hide
          */
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
@@ -10369,6 +10379,11 @@ public final class Settings {
          */
         @Readable
         public static final String NOTIFICATION_DISMISS_RTL = "notification_dismiss_rtl";
+
+        /**
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Comma separated list of QS tiles that have been auto-added already.
