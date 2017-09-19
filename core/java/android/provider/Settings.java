e47948ffe69b76103a6838244d2c12f7ea6f58b6
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Change-Id: Ib65e075bbbc1e3f2ff8d98ab0770f827b9c397bc
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5861,6 +5861,12 @@ public final class Settings {
          */
         public static final String ENABLE_TASKBAR = "enable_taskbar";
 
+	/**
+         * Whether to display the torch option in the power menu
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
