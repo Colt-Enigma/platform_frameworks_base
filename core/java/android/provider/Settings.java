7da711ba5556fe7a025e56de41f008fd55d051b1
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Change-Id: Ib65e075bbbc1e3f2ff8d98ab0770f827b9c397bc
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5433,6 +5433,12 @@ public final class Settings {
         public static final String POWERMENU_EMERGENCY = "powermenu_emergency";
 
         /**
+         * Whether to display the torch option in the power menu
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
          * Which applications to disable heads up notifications for
          *
          * @hide
