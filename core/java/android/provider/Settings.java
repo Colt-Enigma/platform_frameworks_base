b52057dc78c76eeb6a62d296f1bee174f521bdfc
Marko Man <darkobas@gmail.com>
2019-09-24 14:46:01 +0200

    base: Double tap to sleep on statusbar [1/2]
    arrow-11.0 edits:
    - Adapt code to refactored statusbar controllers.
    
    wave edits:
    - Adapt to existing dt2s on lockscreen
    
    commit f3321f6a498b3b55a509524183ce83b7503cf936
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed May 23 21:55:04 2018 +0200
    
        base: SystemUI: prevent that double tap to sleep triggers panel expand
    
        arrow-11.0 edits:
            - Adapt code to refactored statusbar controllers.
    
        http://jira.omnirom.org/browse/OMNI-1466
    
        Change-Id: If304577d666ebf8423cca1298dfbe044843657cc
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    
    [ghostrider-reborn | imjyotiraditya] adapted to android 12
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ibfcd4619654910a95e6d49acbbceb15e21e2d2d0
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5517,7 +5517,13 @@ public final class Settings {
         @Readable
         public static final String DESKTOP_MODE = "desktop_mode";
 
-	/**
+        /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * @hide
          */
         public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
@@ -5721,6 +5727,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CONNECT);
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,5 +90,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.QS_PANEL_STYLE
+        Settings.System.DOUBLE_TAP_SLEEP_GESTURE
     };
 }
