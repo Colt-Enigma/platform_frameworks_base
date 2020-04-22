5954a828d15bb80729068abf40cc82d31cd7c0ac
Chris Crump <chris@cypheros.co>
2020-04-22 14:24:18 -0400

    SystemUI: Block face authentication when device is in pocket mode
    Use pocket lock to dertermine if the device is in pocket, don't listen for face authentication if so. This helps not triggering failed attempts
    
    Change-Id: Iae290dd81ec1370e26ef1c0648b7c7c1cc413906
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1738,7 +1738,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                         handleKeyguardGoingAway((boolean) msg.obj);
                         break;
                     case MSG_POCKET_STATE_CHANGED:
-                        updateFingerprintListeningState();
+                        updateBiometricListeningState();
                         break;
                     default:
                         super.handleMessage(msg);
@@ -1994,7 +1994,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         BiometricAuthenticated face = mUserFaceAuthenticated.get(getCurrentUser());
         return mAssistantVisible && mKeyguardOccluded
                 && !(face != null && face.mAuthenticated)
-                && !mUserHasTrust.get(getCurrentUser(), false) && !mIsDeviceInPocket;
+                && !mUserHasTrust.get(getCurrentUser(), false);
     }
 
     private boolean shouldListenForFingerprint() {
@@ -2056,7 +2056,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 && !mSwitchingUser && !isFaceDisabled(user) && becauseCannotSkipBouncer
                 && !mKeyguardGoingAway && mFaceSettingEnabledForUser.get(user) && !mLockIconPressed
                 && strongAuthAllowsScanning && mIsPrimaryUser
-                && !mSecureCameraLaunched;
+                && !mSecureCameraLaunched && !mIsDeviceInPocket;
 
         if (shouldListen && mFaceAuthOnlyOnSecurityView && !mBouncerFullyShown){
             shouldListen = false;
