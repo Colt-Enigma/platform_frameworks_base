dfc78ff5961172198e8b90b37e23a9ca2bebd338
Anay Wadhera <awadhera@berkeley.edu>
2020-07-21 00:28:29 -0700

    base: clipboard toast toggle [1/2]
    Change-Id: I1c8c47b1865eae864e44f4e0b682dee4fe07c827
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6514,6 +6514,12 @@ public final class Settings {
         public static final String VISUALIZER_CENTER_MIRRORED = "visualizer_center_mirrored";
 
         /**
+         * Toggle for clipboard access toast.
+         * @hide
+         */
+        public static final String SHOW_CLIPBOARD_TOAST = "show_clipboard_toast";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
