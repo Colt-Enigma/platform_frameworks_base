5f710d514654668758f6f772883803a64d4221f2
Zhuying Li <vend_wsd_am_027@mediatek.com>
2020-10-13 14:04:21 +0800

    Use name instead of package name
    Can't get package setting info with wrong package name.
    The API toStaticSharedLibraryPackageName will build correct package name
    with correct information.
    
    Bug: 170699390
    Bug: 172232086
    Test: manual test pass.
    Change-Id: Iad97a3adf19ec37797123070a0662bfc7fc3337c
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -10680,7 +10680,7 @@ public class PackageManagerService extends IPackageManager.Stub
                     continue;
                 }
                 final PackageSetting staticLibPkgSetting = getPackageSetting(
-                        toStaticSharedLibraryPackageName(sharedLibraryInfo.getPackageName(),
+                        toStaticSharedLibraryPackageName(sharedLibraryInfo.getName(),
                                 sharedLibraryInfo.getLongVersion()));
                 if (staticLibPkgSetting == null) {
                     Slog.wtf(TAG, "Shared lib without setting: " + sharedLibraryInfo);
