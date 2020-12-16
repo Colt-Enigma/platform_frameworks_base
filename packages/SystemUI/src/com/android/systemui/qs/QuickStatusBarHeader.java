c53b47d3b96acd58a1e221080f8162dbbb8f3c73
calebcabob <calphonic@gmail.com>
2020-12-16 12:30:25 -0500

    QuickStatusBarHeader: Long-press header clock to access settings
    Most folks won't even realize this is there. Also not
    sure how useful this will be for others. This is
    mostly just personal preference.
    
    idoybh: Improve some code - Localize the longclick listener, we don't
    need to implement it for the whole class just yet
    
    Signed-off-by: calebcabob <calphonic@gmail.com>
    Change-Id: I6caac2d6997233243c0fcdceea47b8ef7c9c254d
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -35,6 +35,8 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.AlarmClock;
 import android.provider.CalendarContract;
 import android.provider.Settings;
@@ -132,6 +134,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private final StatusBarIconController mStatusBarIconController;
     private final ActivityStarter mActivityStarter;
     private final BlurUtils mBlurUtils;
+    private final Vibrator mVibrator;
 
     private QSPanel mQsPanel;
 
@@ -252,6 +255,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mUiEventLogger = uiEventLogger;
         mBroadcastDispatcher = broadcastDispatcher;
         mBlurUtils = new BlurUtils(mContext.getResources(), new DumpManager());
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override
@@ -311,7 +315,14 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
-	mClockView.setQsHeader();
+        mClockView.setQsHeader();
+        mClockView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    startDateTimeSettingsActivity();
+                    return false;
+                }
+            });
         mDateView = findViewById(R.id.date);
         mDateView.setOnClickListener(this);
         mDataUsageLayout = findViewById(R.id.daily_data_usage_layout);
@@ -830,6 +841,14 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
                 Intent.ACTION_POWER_USAGE_SUMMARY), 0);
         }
+    }
+
+    private void startDateTimeSettingsActivity() {
+        Intent nIntent = new Intent(Intent.ACTION_MAIN);
+        nIntent.setClassName("com.android.settings",
+            "com.android.settings.Settings$DateTimeSettingsActivity");
+        mActivityStarter.startActivity(nIntent, true /* dismissShade */);
+        mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     @Override
