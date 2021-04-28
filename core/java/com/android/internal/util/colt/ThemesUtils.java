30e1121befe2be4bc5d1e47cf6f87ac65c582059
Advaith Bhat <advaithbhat9@gmail.com>
2021-04-28 08:02:35 +0000

    SystemUI: Add 5 more Custom Statusbar Signal & Wi-Fi icons [1/2]
    - Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I10dc037f2709ce41ba1b451312ee20dfdcf1b799
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/com/android/internal/util/colt/ThemesUtils.java
+++ core/java/com/android/internal/util/colt/ThemesUtils.java
@@ -185,6 +185,11 @@ public class ThemesUtils {
         "org.blissroms.systemui.signalbar_a",
         "org.blissroms.systemui.signalbar_b",
         "org.blissroms.systemui.signalbar_c",
+        "org.blissroms.systemui.signalbar_d",
+        "org.blissroms.systemui.signalbar_e",
+        "org.blissroms.systemui.signalbar_f",
+        "org.blissroms.systemui.signalbar_g",
+        "org.blissroms.systemui.signalbar_h",
     };
 
     // Statusbar Wifi icons
@@ -192,5 +197,10 @@ public class ThemesUtils {
         "org.blissroms.systemui.wifibar_a",
         "org.blissroms.systemui.wifibar_b",
         "org.blissroms.systemui.wifibar_c",
+        "org.blissroms.systemui.wifibar_d",
+        "org.blissroms.systemui.wifibar_e",
+        "org.blissroms.systemui.wifibar_f",
+        "org.blissroms.systemui.wifibar_g",
+        "org.blissroms.systemui.wifibar_h",
     };
 }
