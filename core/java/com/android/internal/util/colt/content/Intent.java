9a95aa206c082bf8a5fba8cbb80294948deec3ae
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-08-08 15:24:49 +0530

    PowerManagerService: Send broadcast when going to sleep

---
+++ core/java/com/android/internal/util/colt/content/Intent.java
@@ -0,0 +1,15 @@
+package com.android.internal.util.colt.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "colt.intent.action.GO_TO_SLEEP";
+}
