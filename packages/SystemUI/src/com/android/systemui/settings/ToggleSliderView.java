0ff580ef4e9fad0bf7c0c5f157f764480938daa1
RakeshBatra <raakesh.batra@rediffmail.com>
2021-09-15 09:17:06 -0700

    Revert "SystemUI: Animate brightness slider"
    This reverts commit af8e01124e2e9d11fa1997940bf0bf2fca258d7a.
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/ToggleSliderView.java
+++ packages/SystemUI/src/com/android/systemui/settings/ToggleSliderView.java
@@ -202,7 +202,6 @@ public class ToggleSliderView extends RelativeLayout implements ToggleSlider {
 
             if (mMirrorController != null) {
                 mMirrorController.hideMirror();
-                mMirrorController.hideMirrorImmediately();
             }
         }
     };
