880cef8b6f957230345d359d403af579aa2e132a
CrisBal <crisbaladim@yahoo.gr>
2021-09-18 07:24:52 +0300

    Animations: Match the default values
    *Animations: Match the default fwb values with Settings values according commit
    9346b44
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -948,9 +948,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    private float mWindowAnimationScaleSetting = 0.8f;
-    private float mTransitionAnimationScaleSetting = 0.8f;
-    private float mAnimatorDurationScaleSetting = 0.8f;
+    private float mWindowAnimationScaleSetting = 0.80f;
+    private float mTransitionAnimationScaleSetting = 0.80f;
+    private float mAnimatorDurationScaleSetting = 0.80f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
     private boolean mAnimationsForceDisabled = false;
