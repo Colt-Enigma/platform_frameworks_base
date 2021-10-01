Project-Xtended/frameworks_base

Status bar colors [1/2]
SDB: Bring back the Dead. As Of Now -

- Battery & Text color, (also battery text when on lock screen and charging)
- Notifications Icon color
- Battery & Text color (dark mode), normal status bar only
- Notification Icon color (dark mode), normal status bar only

core/java/com/android/internal/util/xtended/ColorConstants.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.xtended;
+
+public class ColorConstants {
+
+    // Theme general
+    public static final int WHITE = 0xffffffff;
+    public static final int BLACK = 0xff000000;
+
+    // SystemUI
+    public static final int LIGHT_MODE_COLOR_SINGLE_TONE          = WHITE;
+    public static final int LIGHT_MODE_COLOR_DUAL_TONE_BACKGROUND = 0x4dffffff;
+    public static final int LIGHT_MODE_COLOR_DUAL_TONE_FILL       = WHITE;
+
+    public static final int DARK_MODE_COLOR_SINGLE_TONE          = 0x99000000;
+    public static final int DARK_MODE_COLOR_DUAL_TONE_BACKGROUND = 0x3d000000;
+    public static final int DARK_MODE_COLOR_DUAL_TONE_FILL       = 0x7a000000;
+
+    // Alpha values
+
+    // General
+    public static final int FULLY_OPAQUE_ALPHA       = 255;
+
+    // General (Night theme)
+    public static final int TEXT_PRIMARY_ALPHA_NIGHT   = FULLY_OPAQUE_ALPHA;
+    public static final int TEXT_SECONDARY_ALPHA_NIGHT = 179;
+    public static final int ICON_NORMAL_ALPHA_NIGHT    = TEXT_SECONDARY_ALPHA_NIGHT;
+
+    // General (Day theme)
+    public static final int TEXT_PRIMARY_ALPHA_DAY   = 222;
+    public static final int TEXT_SECONDARY_ALPHA_DAY = 138;
+    public static final int ICON_NORMAL_ALPHA_DAY    = TEXT_SECONDARY_ALPHA_DAY;
+
+    // SystemUI
+    public static final int LIGHT_MODE_ALPHA_SINGLE_TONE          = FULLY_OPAQUE_ALPHA;
+    public static final int LIGHT_MODE_ALPHA_DUAL_TONE_BACKGROUND = 77;
+    public static final int LIGHT_MODE_ALPHA_DUAL_TONE_FILL       = FULLY_OPAQUE_ALPHA;
+
+    public static final int DARK_MODE_ALPHA_SINGLE_TONE          = 153;
+    public static final int DARK_MODE_ALPHA_DUAL_TONE_BACKGROUND = 61;
+    public static final int DARK_MODE_ALPHA_DUAL_TONE_FILL       = 122;
+}
+

packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -53,6 +53,7 @@ import android.widget.TextView;
 
 import androidx.annotation.StyleRes;
 
+import com.android.internal.util.xtended.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.settingslib.graph.CircleBatteryDrawable;
 import com.android.settingslib.graph.FullCircleBatteryDrawable;
@@ -238,7 +239,7 @@ public class BatteryMeterView extends LinearLayout implements
      *
      * @param shouldUseWallpaperTextColor whether we should use wallpaperTextColor for all
      *                                    components
-     */
+     
     public void useWallpaperTextColor(boolean shouldUseWallpaperTextColor) {
         if (shouldUseWallpaperTextColor == mUseWallpaperTextColors) {
             return;
@@ -256,6 +257,7 @@ public class BatteryMeterView extends LinearLayout implements
                     mNonAdaptedSingleToneColor);
         }
     }
+*/
 
     public void setColorsFromContext(Context context) {
         if (context == null) {
@@ -599,17 +601,27 @@ public class BatteryMeterView extends LinearLayout implements
 
     @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
-        float intensity = DarkIconDispatcher.isInArea(area, this) ? darkIntensity : 0;
-        mNonAdaptedSingleToneColor = mDualToneHandler.getSingleColor(intensity);
-        mNonAdaptedForegroundColor = mDualToneHandler.getFillColor(intensity);
-        mNonAdaptedBackgroundColor = mDualToneHandler.getBackgroundColor(intensity);
+        mNonAdaptedSingleToneColor = StatusBarColorHelper.getTextSingleToneTint(getContext(), area, this,
+                    darkIntensity);
+        mNonAdaptedForegroundColor = StatusBarColorHelper.getIconDualToneFillTint(getContext(), area, this,
+                    darkIntensity);
+        mNonAdaptedBackgroundColor = StatusBarColorHelper.getIconDualToneBackgroundTint(getContext(), area, this,
+                    darkIntensity);
 
-        if (!mUseWallpaperTextColors) {
-            updateColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor,
-                    mNonAdaptedSingleToneColor);
+        mThemedDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mCircleDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mFullCircleDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mTextColor = mNonAdaptedSingleToneColor;
+        if (mBatteryPercentView != null) {
+            mBatteryPercentView.setTextColor(mNonAdaptedSingleToneColor);
+        }
+
+        if (mUnknownStateDrawable != null) {
+            mUnknownStateDrawable.setTint(mNonAdaptedSingleToneColor);
         }
     }
 
+/**
     private void updateColors(int foregroundColor, int backgroundColor, int singleToneColor) {
         mThemedDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
         mCircleDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
@@ -623,6 +635,7 @@ public class BatteryMeterView extends LinearLayout implements
             mUnknownStateDrawable.setTint(singleToneColor);
         }
     }
+*/
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         String powerSave = mThemedDrawable == null ? null : mThemedDrawable.getPowerSaveEnabled() + "";

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -50,6 +50,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.xtended.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
@@ -491,13 +492,15 @@ public class KeyguardStatusBarView extends RelativeLayout
         onThemeChanged();
     }
 
-    private void updateIconsAndTextColors() {
-        @ColorInt int textColor = Utils.getColorAttrDefaultColor(mContext,
+    public void updateIconsAndTextColors() {
+        @ColorInt int wallpaperTextColor = Utils.getColorAttrDefaultColor(mContext,
                 R.attr.wallpaperTextColor);
-        @ColorInt int iconColor = Utils.getColorStateListDefaultColor(mContext,
-                Color.luminance(textColor) < 0.5 ? R.color.dark_mode_icon_color_single_tone :
-                R.color.light_mode_icon_color_single_tone);
-        float intensity = textColor == Color.WHITE ? 0 : 1;
+        float intensity = wallpaperTextColor == Color.WHITE ? 0 : 1;
+        @ColorInt int textColor = StatusBarColorHelper.getTextSingleToneTint(getContext(), mEmptyRect, this,
+                    intensity);
+        @ColorInt int iconColor = StatusBarColorHelper.getIconSingleToneTint(getContext(), mEmptyRect, this,
+                    intensity);
+
         mCarrierLabel.setTextColor(iconColor);
         if (mIconManager != null) {
             mIconManager.setTint(iconColor);

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -16,6 +16,7 @@ import androidx.collection.ArrayMap;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.util.ContrastColorUtil;
+import com.android.internal.util.xtended.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -63,6 +64,7 @@ public class NotificationIconAreaController implements DarkReceiver,
     private int mIconTint = Color.WHITE;
     private int mCenteredIconTint = Color.WHITE;
 
+    private float mDarkIntensity = 0f;
     private StatusBar mStatusBar;
     protected View mNotificationIconArea;
     private NotificationIconContainer mNotificationIcons;
@@ -222,6 +224,7 @@ public class NotificationIconAreaController implements DarkReceiver,
      * @param darkIntensity
      */
     public void onDarkChanged(Rect tintArea, float darkIntensity, int iconTint) {
+        mDarkIntensity = darkIntensity;
         if (tintArea == null) {
             mTintArea.setEmpty();
         } else {
@@ -514,7 +517,7 @@ public class NotificationIconAreaController implements DarkReceiver,
         int color = StatusBarIconView.NO_COLOR;
         boolean colorize = !isPreL || NotificationUtils.isGrayscale(v, mContrastColorUtil);
         if (colorize) {
-            color = DarkIconDispatcher.getTint(mTintArea, v, tint);
+            color = StatusBarColorHelper.getIconSingleToneTint(mContext, mTintArea, v, mDarkIntensity);
         }
         v.setStaticDrawableColor(color);
         v.setDecorColor(tint);

Colt-Enigma/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/com/android/internal/util/colt/ColorConstants.java
@@ -1,0 +1,58 @@
+/*
+* Copyright (C) 2018 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.colt;
+
+public class ColorConstants {
+
+    // Theme general
+    public static final int WHITE = 0xffffffff;
+    public static final int BLACK = 0xff000000;
+
+    // SystemUI
+    public static final int LIGHT_MODE_COLOR_SINGLE_TONE          = WHITE;
+    public static final int LIGHT_MODE_COLOR_DUAL_TONE_BACKGROUND = 0x4dffffff;
+    public static final int LIGHT_MODE_COLOR_DUAL_TONE_FILL       = WHITE;
+
+    public static final int DARK_MODE_COLOR_SINGLE_TONE          = 0x99000000;
+    public static final int DARK_MODE_COLOR_DUAL_TONE_BACKGROUND = 0x3d000000;
+    public static final int DARK_MODE_COLOR_DUAL_TONE_FILL       = 0x7a000000;
+
+    // Alpha values
+
+    // General
+    public static final int FULLY_OPAQUE_ALPHA       = 255;
+
+    // General (Night theme)
+    public static final int TEXT_PRIMARY_ALPHA_NIGHT   = FULLY_OPAQUE_ALPHA;
+    public static final int TEXT_SECONDARY_ALPHA_NIGHT = 179;
+    public static final int ICON_NORMAL_ALPHA_NIGHT    = TEXT_SECONDARY_ALPHA_NIGHT;
+
+    // General (Day theme)
+    public static final int TEXT_PRIMARY_ALPHA_DAY   = 222;
+    public static final int TEXT_SECONDARY_ALPHA_DAY = 138;
+    public static final int ICON_NORMAL_ALPHA_DAY    = TEXT_SECONDARY_ALPHA_DAY;
+
+    // SystemUI
+    public static final int LIGHT_MODE_ALPHA_SINGLE_TONE          = FULLY_OPAQUE_ALPHA;
+    public static final int LIGHT_MODE_ALPHA_DUAL_TONE_BACKGROUND = 77;
+    public static final int LIGHT_MODE_ALPHA_DUAL_TONE_FILL       = FULLY_OPAQUE_ALPHA;
+
+    public static final int DARK_MODE_ALPHA_SINGLE_TONE          = 153;
+    public static final int DARK_MODE_ALPHA_DUAL_TONE_BACKGROUND = 61;
+    public static final int DARK_MODE_ALPHA_DUAL_TONE_FILL       = 122;
+}
+

packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -53,6 +53,7 @@ import android.widget.TextView;
 
 import androidx.annotation.StyleRes;
 
+import com.android.internal.util.colt.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.settingslib.graph.CircleBatteryDrawable;
 import com.android.settingslib.graph.FullCircleBatteryDrawable;
@@ -248,7 +249,7 @@ public class BatteryMeterView extends LinearLayout implements
      *
      * @param shouldUseWallpaperTextColor whether we should use wallpaperTextColor for all
      *                                    components
-     */
+     
     public void useWallpaperTextColor(boolean shouldUseWallpaperTextColor) {
         if (shouldUseWallpaperTextColor == mUseWallpaperTextColors) {
             return;
@@ -266,6 +267,7 @@ public class BatteryMeterView extends LinearLayout implements
                     mNonAdaptedSingleToneColor);
         }
     }
+*/
 
     public void setColorsFromContext(Context context) {
         if (context == null) {
@@ -634,18 +636,28 @@ public class BatteryMeterView extends LinearLayout implements
 
     @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
-        float intensity = DarkIconDispatcher.isInArea(area, this) ? darkIntensity : 0;
-        mNonAdaptedSingleToneColor = mDualToneHandler.getSingleColor(intensity);
-        mNonAdaptedForegroundColor = mDualToneHandler.getFillColor(intensity);
-        mNonAdaptedBackgroundColor = mDualToneHandler.getBackgroundColor(intensity);
+        mNonAdaptedSingleToneColor = StatusBarColorHelper.getTextSingleToneTint(getContext(), area, this,
+                    darkIntensity);
+        mNonAdaptedForegroundColor = StatusBarColorHelper.getIconDualToneFillTint(getContext(), area, this,
+                    darkIntensity);
+        mNonAdaptedBackgroundColor = StatusBarColorHelper.getIconDualToneBackgroundTint(getContext(), area, this,
+                    darkIntensity);
 
-        if (!mUseWallpaperTextColors) {
-            updateColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor,
-                    mNonAdaptedSingleToneColor);
+        mThemedDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mCircleDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mFullCircleDrawable.setColors(mNonAdaptedForegroundColor, mNonAdaptedBackgroundColor, mNonAdaptedSingleToneColor);
+        mTextColor = mNonAdaptedSingleToneColor;
+        if (mBatteryPercentView != null) {
+            mBatteryPercentView.setTextColor(mNonAdaptedSingleToneColor);
+        }
+
+        if (mUnknownStateDrawable != null) {
+            mUnknownStateDrawable.setTint(mNonAdaptedSingleToneColor);
         }
     }
 
-    public void updateColors(int foregroundColor, int backgroundColor, int singleToneColor) {
+/**
+    private void updateColors(int foregroundColor, int backgroundColor, int singleToneColor) {
         mThemedDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
         mRLandscapeDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
         mLandscapeDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
@@ -660,6 +672,7 @@ public class BatteryMeterView extends LinearLayout implements
             mUnknownStateDrawable.setTint(singleToneColor);
         }
     }
+*/
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         String powerSave = mThemedDrawable == null ? null : mThemedDrawable.getPowerSaveEnabled() + "";

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -47,6 +47,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.colt.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.Dependency;
@@ -493,13 +494,15 @@ public class KeyguardStatusBarView extends RelativeLayout
         onThemeChanged();
     }
 
-    private void updateIconsAndTextColors() {
-        @ColorInt int textColor = Utils.getColorAttrDefaultColor(mContext,
+    public void updateIconsAndTextColors() {
+        @ColorInt int wallpaperTextColor = Utils.getColorAttrDefaultColor(mContext,
                 R.attr.wallpaperTextColor);
-        @ColorInt int iconColor = Utils.getColorStateListDefaultColor(mContext,
-                Color.luminance(textColor) < 0.5 ? R.color.dark_mode_icon_color_single_tone :
-                R.color.light_mode_icon_color_single_tone);
-        float intensity = textColor == Color.WHITE ? 0 : 1;
+        float intensity = wallpaperTextColor == Color.WHITE ? 0 : 1;
+        @ColorInt int textColor = StatusBarColorHelper.getTextSingleToneTint(getContext(), mEmptyRect, this,
+                    intensity);
+        @ColorInt int iconColor = StatusBarColorHelper.getIconSingleToneTint(getContext(), mEmptyRect, this,
+                    intensity);
+
         mCarrierLabel.setTextColor(iconColor);
         if (mIconManager != null) {
             mIconManager.setTint(iconColor);

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -16,6 +16,7 @@ import androidx.collection.ArrayMap;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.util.ContrastColorUtil;
+import com.android.internal.util.colt.StatusBarColorHelper;
 import com.android.settingslib.Utils;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
@@ -63,6 +64,7 @@ public class NotificationIconAreaController implements DarkReceiver,
     private int mIconTint = Color.WHITE;
     private int mCenteredIconTint = Color.WHITE;
 
+    private float mDarkIntensity = 0f;
     private StatusBar mStatusBar;
     protected View mNotificationIconArea;
     private NotificationIconContainer mNotificationIcons;
@@ -222,6 +224,7 @@ public class NotificationIconAreaController implements DarkReceiver,
      * @param darkIntensity
      */
     public void onDarkChanged(Rect tintArea, float darkIntensity, int iconTint) {
+        mDarkIntensity = darkIntensity;
         if (tintArea == null) {
             mTintArea.setEmpty();
         } else {
@@ -514,7 +517,7 @@ public class NotificationIconAreaController implements DarkReceiver,
         int color = StatusBarIconView.NO_COLOR;
         boolean colorize = !isPreL || NotificationUtils.isGrayscale(v, mContrastColorUtil);
         if (colorize) {
-            color = DarkIconDispatcher.getTint(mTintArea, v, tint);
+            color = StatusBarColorHelper.getIconSingleToneTint(mContext, mTintArea, v, mDarkIntensity);
         }
         v.setStaticDrawableColor(color);
         v.setDecorColor(tint);