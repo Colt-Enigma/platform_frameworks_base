afe59b833a85470b8b0468f43c9c391d3828b6a5
RakeshBatra <raakesh.batra@rediffmail.com>
2021-11-06 20:21:50 -0700

    Screenshot Tile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -28,7 +28,7 @@ import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
 
-import com.android.internal.util.aospextended.AEXUtils;
+import com.android.internal.util.colt.ColtUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -65,7 +65,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EXTENSIONS;
+        return MetricsEvent.COLT;
     }
 
     @Override
@@ -91,7 +91,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         try {
              Thread.sleep(1000); //1s
         } catch (InterruptedException ie) {}
-        AEXUtils.takeScreenshot(mRegion ? false : true);
+        ColtUtils.takeScreenshot(mRegion ? false : true);
     }
 
     @Override
