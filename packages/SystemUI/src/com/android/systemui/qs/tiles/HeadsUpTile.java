0bca65f1a88dfeebdf2d542ff77c5aed12087967
RakeshBatra <raakesh.batra@rediffmail.com>
2021-11-06 20:22:47 -0700

    Headsuptile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -140,7 +140,7 @@ public class HeadsUpTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EXTENSIONS;
+        return MetricsEvent.COLT;
     }
 
     @Override
