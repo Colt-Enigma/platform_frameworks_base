3c56225c549100598c6553c074eca6ac91366cc5
StarkDroid <gamerprince.exp@gmail.com>
2022-08-14 07:07:53 +0800

    Statusbar Clock background chip [1/2]
    * Inspired by looking at few magisk/substratum module
    
    [@neobuddy89: Fixed up clock paddings]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7038,6 +7038,14 @@ public final class Settings {
         public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
 
         /**
+         * Statusbar clock background
+         * 0 - hide accented chip  (default)
+         * 1 - show accented chip
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_CHIP = "statusbar_clock_chip";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
