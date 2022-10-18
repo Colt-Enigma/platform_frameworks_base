eb9dc54d1f64649a61b6421b772b52c09783f51a
RakeshBatra <raakesh.batra@rediffmail.com>
2022-10-18 08:58:38 -0700

    Revert "SystemUI: BrightnessController: do not update slider from DisplayManager callback if user is changing brightness"
    This reverts commit 873045f657435e760f90c7f7050b0280451fb10c.
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -96,7 +96,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
 
         @Override
         public void onDisplayChanged(int displayId) {
-            if (mUserChangedBrightness) return;
             mBackgroundHandler.post(mUpdateSliderRunnable);
         }
     };
@@ -110,7 +109,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private float mBrightnessMax = PowerManager.BRIGHTNESS_MAX;
 
     private ValueAnimator mSliderAnimator;
-    private boolean mUserChangedBrightness;
 
     @Override
     public void setMirror(BrightnessMirrorController controller) {
@@ -382,7 +380,6 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     BrightnessSynchronizer.brightnessFloatToInt(valFloat));
 
         }
-        mUserChangedBrightness = tracking && !stopTracking;
         setBrightness(valFloat);
         if (!tracking) {
             AsyncTask.execute(new Runnable() {
