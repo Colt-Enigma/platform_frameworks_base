4dd695172d8f1fcc3d2005aa79c40700959f4606
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-24 22:38:00 +0530

    PixelPropsUtils: Allow spoofing device model for Netflix
    It can be used to unlock HDR for devices that don't
    have it by default.
    
    Set config_netflixSpoofModel to the desired model in
    device overlay.
    
    * Adapt to current PixelPropsUtils
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -18,9 +18,12 @@
 package com.android.internal.util.custom;
 
 import android.app.Application;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
+
+import com.android.internal.R;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -158,6 +161,9 @@ public class PixelPropsUtils {
             "bramble",
             "sunfish"
     };
+
+    private static final String sNetflixModel =
+            Resources.getSystem().getString(R.string.config_netflixSpoofModel);
 
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
@@ -297,6 +303,11 @@ public class PixelPropsUtils {
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")) {
             setPropValue("FINGERPRINT", Build.VERSION.INCREMENTAL);
+            return;
+        }
+        if (!sNetflixModel.isEmpty() && packageName.equals("com.netflix.mediaclient")) {
+            if (DEBUG) Log.d(TAG, "Setting model to " + sNetflixModel + " for Netflix");
+            setPropValue("MODEL", sNetflixModel);
             return;
         }
     }
