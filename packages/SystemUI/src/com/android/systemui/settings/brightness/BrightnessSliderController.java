3d08138acf118e1f3576fd64e98d457893907e43
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-31 14:57:04 +0800

    SystemUI: Avoid ClassCastException for auto brightness icon
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderController.java
@@ -24,7 +24,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
@@ -53,7 +53,7 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
 
     private Listener mListener;
     private ToggleSlider mMirror;
-    private ImageView mIcon;
+    private ImageButton mIcon;
     private BrightnessMirrorController mMirrorController;
     private boolean mTracking;
     private final FalsingManager mFalsingManager;
@@ -92,7 +92,7 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
         return mView;
     }
 
-    public ImageView getIcon() {
+    public ImageButton getIcon() {
         return mIcon;
     }
 
