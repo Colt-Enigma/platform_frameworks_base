696b1e55ee529893fb006c31175bf53eba37911d
RakeshBatra <raakesh.batra@rediffmail.com>
2023-01-08 08:13:02 -0800

    more corrections

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -224,7 +224,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.ARROW;
+        return MetricsEvent.COLT;
     }
 
     /**
