20e0b2303997fb22760ddd3de9d6bd1c1222d90e
RakeshBatra <raakesh.batra@rediffmail.com>
2023-01-08 08:25:57 -0800

    Revert "Screenshot: Unregister task listener on destroy"
    This reverts commit 45248362976fb9e3db6128fdbdf73a77cb0a4dd7.
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -498,7 +498,6 @@ public class ScreenshotController {
         removeWindow();
         releaseMediaPlayer();
         releaseContext();
-        TaskStackChangeListeners.getInstance().unregisterTaskStackListener(mTaskListener);
         mBgExecutor.shutdownNow();
     }
 
