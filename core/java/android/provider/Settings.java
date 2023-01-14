2bba6ef5cc6473a60f19cb2e644fcac23bbd5eeb
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-14 19:56:01 +0200

    base: Allow setting bottom keyguard shortcuts [1/2]
    Allows changing the priority, sides and to disable
    
    Change-Id: Idc2dc9469c8857f59b23996232572a8634f59d5e
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5661,6 +5661,17 @@ public final class Settings {
         public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
 
         /**
+         * Bottom screen shortcuts on keyguard
+         * Two lists of strings delimeted by ;
+         * Each list of string is delimited by ,
+         * Valid strings are: home, wallet, qr, camera, flashlight and none
+         * The order in each list decides the priority for each shortcut
+         * @hide
+         */
+        @Readable
+        public static final String KEYGUARD_QUICK_TOGGLES = "keyguard_quick_toggles";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
