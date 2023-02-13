7353c795e50bf6cec3803dcfe2ad6552aed31f99
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-14 07:04:27 +0800

    hwui: Prevent a null pointer on renderthread
    02-13 18:30:27.831 22315 22315 F DEBUG   : Cause: null pointer dereference
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x0  b400007ada6a6600  x1  0000007aea3d4a10  x2  b400007ada57a998  x3  0000000000000001
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x4  0000007aea3d3f80  x5  b400007abe1f22e0  x6  0000000000000001  x7  0000000000000010
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x8  0000000000000000  x9  d3dfa7cb4870f3e0  x10 b400007ada6a6200  x11 0000000000000019
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x12 0000000000000050  x13 0000000000000033  x14 b400007ab9559000  x15 b400007ab9559000
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x16 0000007c048d96a0  x17 0000007c26d1a2f0  x18 0000007aea2ca000  x19 b400007ada6a6600
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x20 b400007ada6a6628  x21 b400007ada6a6600  x22 b400007ad4802e90  x23 b400007ad4802e80
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x24 7ffffffffffffff8  x25 1fffffffffffffff  x26 00000000ffffffff  x27 b400007b61ab02a8
    02-13 18:30:27.831 22315 22315 F DEBUG   :     x28 b400007b61ab02a8  x29 0000007aea3d47d0
    02-13 18:30:27.831 22315 22315 F DEBUG   :     lr  0000007c173f6eb0  sp  0000007aea3d47d0  pc  0000007c173f8b40  pst 0000000020001000
    02-13 18:30:27.831 22315 22315 F DEBUG   : backtrace:
    02-13 18:30:27.831 22315 22315 F DEBUG   :       #00 pc 0000000000249b40  /system/lib64/libhwui.so (android::uirenderer::AnimatorManager::pushStaging()+112) (BuildId: 31073a6e7866492366c7945e840d3e74)
    02-13 18:30:27.831 22315 22315 F DEBUG   :       #01 pc 0000000000247eac  /system/lib64/libhwui.so (android::uirenderer::AnimationContext::runRemainingAnimations(android::uirenderer::TreeInfo&)+44) (BuildId: 31073a6e7866492366c7945e840d3e74)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- libs/hwui/AnimatorManager.cpp
+++ libs/hwui/AnimatorManager.cpp
@@ -86,12 +86,16 @@ void AnimatorManager::pushStaging() {
 
     if (mCancelAllAnimators) {
         for (auto& animator : mAnimators) {
-            animator->forceEndNow(mAnimationHandle->context());
+            if (animator) {
+                animator->forceEndNow(mAnimationHandle->context());
+            }
         }
         mCancelAllAnimators = false;
     } else {
         for (auto& animator : mAnimators) {
-            animator->pushStaging(mAnimationHandle->context());
+            if (animator) {
+                animator->pushStaging(mAnimationHandle->context());
+            }
         }
     }
 }
@@ -110,7 +114,9 @@ public:
         *mDirtyMask |= animator->dirtyMask();
         bool remove = animator->animate(mContext);
         if (remove) {
-            animator->detach();
+            if (animator) {
+                animator->detach();
+            }
         } else {
             if (animator->isRunning()) {
                 mInfo.out.hasAnimations = true;
