GeoZac/frameworks_base-1

[SQUASH] base: Custom logo customizations [1/2]
* Original placement logic and custom logo patches
  by LorDClockaN and varund7726 respectively.
* @neobuddy89: Complete rewrite for oreo with darkicon dispatcher.

-AICP'fied by @eyosen:
  * We are AICP after all.
  * Also make naming rom independent.
  * Brought up to Aicp Pie, 10, 11, 12
  * Includes 27dc548f8c483429af93a265492f2e0e7e514112
	fwb: statusbar logo code cleanup
  * and more code cleanup

Change-Id: I9f4a9331a3867712f69579f764ef2cebd0bbe717
Signed-off-by: Julian Veit <claymore1298@gmail.com>

Squashed with the following:

* Clean up and fix Status bar Logo feature
* Clean up Custom logo customizations
* Add amogus icon
* Re-align amogus icon
* Cleanup and add more fresh ones
* Logo: Avoid NPE
* Logo: Adjust onDarkChanged (after 6c5ebb3)

* Set fallback to ic_evolution_logo to prevent NullPointerException
  in case of mLogoStyle doesn't belong to any of below, which
  causes crash and bootloop on drawable.setTint(mTintColor);.

logcat:

10-24 22:35:59.887  3948  3948 E AndroidRuntime: FATAL EXCEPTION: main
10-24 22:35:59.887  3948  3948 E AndroidRuntime: Process: com.android.systemui, PID: 3948
10-24 22:35:59.887  3948  3948 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.graphics.drawable.Drawable.setTint(int)' on a null object reference
10-24 22:35:59.887  3948  3948 E AndroidRuntime:        at com.android.systemui.evolution.logo.LogoImage.updateLogo(LogoImage.java:202)
10-24 22:35:59.887  3948  3948 E AndroidRuntime:        at com.android.systemui.evolution.logo.LogoImage.updateSettings(LogoImage.java:218)
10-24 22:35:59.887  3948  3948 E AndroidRuntime:        at com.android.systemui.evolution.logo.LogoImage.onAttachedToWindow(LogoImage.java:111)
10-24 22:35:59.887  3948  3948 E AndroidRuntime:        at android.view.View.dispatchAttachedToWindow(View.java:21291)
10-24 22:35:59.887  3948  3948 E AndroidRuntime:        at android.view.ViewGroup.dispatchA

packages/SystemUI/src/com/android/systemui/evolution/logo/LogoImage.java
@@ -1,0 +1,224 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.evolution.logo;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+
+import java.util.ArrayList;
+
+public abstract class LogoImage extends ImageView {
+
+    private Context mContext;
+
+    private boolean mAttached;
+
+    private boolean mShowLogo;
+    public int mLogoPosition;
+    private int mLogoStyle;
+    private int mTintColor = Color.WHITE;
+
+    private static final String STATUS_BAR_LOGO =
+            Settings.System.STATUS_BAR_LOGO;
+    private static final String STATUS_BAR_LOGO_POSITION =
+            Settings.System.STATUS_BAR_LOGO_POSITION;
+    private static final String STATUS_BAR_LOGO_STYLE =
+            Settings.System.STATUS_BAR_LOGO_STYLE;
+
+    class SettingsObserver extends ContentObserver {
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO), false, this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO_POSITION),
+                    false, this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO_STYLE),
+                    false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    public LogoImage(Context context) {
+        this(context, null);
+    }
+
+    public LogoImage(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImage(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    protected abstract boolean isLogoHidden();
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mAttached)
+            return;
+
+        mAttached = true;
+
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
+        updateSettings();
+
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (!mAttached)
+            return;
+
+        mAttached = false;
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
+    }
+
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        mTintColor = DarkIconDispatcher.getTint(areas, this, tint);
+        if (mShowLogo && !isLogoHidden()) {
+            updateLogo();
+        }
+    }
+
+    public void updateLogo() {
+        Drawable drawable = null;
+        switch(mLogoStyle){
+            case 0:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_evolution_logo);
+                break;
+            case 1:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_adidas);
+                break;
+            case 2:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_airjordan);
+                break;
+            case 3:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_amogus);
+                break;
+            case 4:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_apple_logo);
+                break;
+            case 5:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_avengers);
+                break;
+            case 6:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_batman);
+                break;
+            case 7:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_batman_tdk);
+                break;
+            case 8:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_beats);
+                break;
+            case 9:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_biohazard);
+                break;
+            case 10:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_blackberry);
+                break;
+            case 11:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_cannabis);
+                break;
+            case 12:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_fire);
+                break;
+            case 13:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_nike);
+                break;
+            case 14:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_pac_man);
+                break;
+            case 15:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_puma);
+                break;
+            case 16:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_rog);
+                break;
+            case 17:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_superman);
+                break;
+            case 18:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_windows);
+                break;
+            case 19:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_xbox);
+                break;
+            default:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_evolution_logo);
+                break;
+        }
+
+        setImageDrawable(null);
+
+        clearColorFilter();
+
+        drawable.setTint(mTintColor);
+        setImageDrawable(drawable);
+    }
+
+    public void updateSettings() {
+        mShowLogo = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO, 0) != 0;
+        mLogoPosition = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_POSITION, 0);
+        mLogoStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        if (!mShowLogo || isLogoHidden()) {
+            setImageDrawable(null);
+            setVisibility(View.GONE);
+            return;
+        }
+        updateLogo();
+        setVisibility(View.VISIBLE);
+    }
+}

Colt-Enigma/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

packages/SystemUI/src/com/android/systemui/colt/logo/LogoImage.java
@@ -1,0 +1,224 @@
+/*
+ * Copyright (C) 2018 crDroid Android Project
+ * Copyright (C) 2018-2019 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.colt.logo;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.R;
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.plugins.DarkIconDispatcher.DarkReceiver;
+
+import java.util.ArrayList;
+
+public abstract class LogoImage extends ImageView {
+
+    private Context mContext;
+
+    private boolean mAttached;
+
+    private boolean mShowLogo;
+    public int mLogoPosition;
+    private int mLogoStyle;
+    private int mTintColor = Color.WHITE;
+
+    private static final String STATUS_BAR_LOGO =
+            Settings.System.STATUS_BAR_LOGO;
+    private static final String STATUS_BAR_LOGO_POSITION =
+            Settings.System.STATUS_BAR_LOGO_POSITION;
+    private static final String STATUS_BAR_LOGO_STYLE =
+            Settings.System.STATUS_BAR_LOGO_STYLE;
+
+    class SettingsObserver extends ContentObserver {
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO), false, this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO_POSITION),
+                    false, this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(STATUS_BAR_LOGO_STYLE),
+                    false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    public LogoImage(Context context) {
+        this(context, null);
+    }
+
+    public LogoImage(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImage(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        final Resources resources = getResources();
+        mContext = context;
+    }
+
+    protected abstract boolean isLogoHidden();
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mAttached)
+            return;
+
+        mAttached = true;
+
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
+        updateSettings();
+
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (!mAttached)
+            return;
+
+        mAttached = false;
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(this);
+    }
+
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        mTintColor = DarkIconDispatcher.getTint(areas, this, tint);
+        if (mShowLogo && !isLogoHidden()) {
+            updateLogo();
+        }
+    }
+
+    public void updateLogo() {
+        Drawable drawable = null;
+        switch(mLogoStyle){
+            case 0:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_colt_logo);
+                break;
+            case 1:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_adidas);
+                break;
+            case 2:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_airjordan);
+                break;
+            case 3:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_amogus);
+                break;
+            case 4:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_apple_logo);
+                break;
+            case 5:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_avengers);
+                break;
+            case 6:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_batman);
+                break;
+            case 7:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_batman_tdk);
+                break;
+            case 8:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_beats);
+                break;
+            case 9:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_biohazard);
+                break;
+            case 10:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_blackberry);
+                break;
+            case 11:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_cannabis);
+                break;
+            case 12:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_fire);
+                break;
+            case 13:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_nike);
+                break;
+            case 14:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_pac_man);
+                break;
+            case 15:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_puma);
+                break;
+            case 16:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_rog);
+                break;
+            case 17:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_superman);
+                break;
+            case 18:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_windows);
+                break;
+            case 19:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_xbox);
+                break;
+            default:
+                drawable = mContext.getResources().getDrawable(R.drawable.ic_colt_logo);
+                break;
+        }
+
+        setImageDrawable(null);
+
+        clearColorFilter();
+
+        drawable.setTint(mTintColor);
+        setImageDrawable(drawable);
+    }
+
+    public void updateSettings() {
+        mShowLogo = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO, 0) != 0;
+        mLogoPosition = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_POSITION, 0);
+        mLogoStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        if (!mShowLogo || isLogoHidden()) {
+            setImageDrawable(null);
+            setVisibility(View.GONE);
+            return;
+        }
+        updateLogo();
+        setVisibility(View.VISIBLE);
+    }
+}