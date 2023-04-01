42302ea179fb591f7fc723503da349c2a3b23ded
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-01 17:05:55 +0800

    SystemUI: Lockscreen clock format settings [1/2]
    Change-Id: I593d98ae8acc3608fe4adeb4a9407429a7a17a55
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6158,6 +6158,12 @@ public final class Settings {
         public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
         /**
+         * Lockscreen clock format
+         * @hide
+         */
+        public static final String CLOCK_USE_CUSTOM_FORMAT = "clock_use_custom_format";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
