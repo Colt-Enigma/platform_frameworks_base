Xtended-Mods/frameworks_base

base: Make IME space optional [1/2]
Co-authored-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>

Change-Id: I6f1fef4c3795e83934cd51936fb7129bfafd3a11
Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
Signed-off-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

libs/WindowManager/Shell/src/com/android/wm/shell/common/DisplayLayout.java
@@ -40,6 +40,7 @@ import android.graphics.Insets;
 import android.graphics.Rect;
 import android.os.UserHandle;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Size;
@@ -89,6 +90,7 @@ public class DisplayLayout {
     private final Rect mStableInsets = new Rect();
     private boolean mHasNavigationBar = false;
     private boolean mHasStatusBar = false;
+    private boolean mIsHideIMESpaceEnabled = true;
     private int mNavBarFrameHeight = 0;
     private boolean mAllowSeamlessRotationDespiteNavBarMoving = false;
     private boolean mNavigationBarCanMove = false;
@@ -165,6 +167,8 @@ public class DisplayLayout {
         rawDisplay.getDisplayInfo(info);
         init(info, context.getResources(), hasNavigationBar(info, context, displayId),
                 hasStatusBar(displayId));
+        mIsHideIMESpaceEnabled = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.HIDE_IME_SPACE_ENABLE , 0, UserHandle.USER_CURRENT) != 0;
     }
 
     public DisplayLayout(DisplayLayout dl) {
@@ -223,7 +227,11 @@ public class DisplayLayout {
         if (mHasStatusBar) {
             convertNonDecorInsetsToStableInsets(res, mStableInsets, mCutout, mHasStatusBar);
         }
-        mNavBarFrameHeight = getNavigationBarFrameHeight(res, mWidth > mHeight);
+        if (mIsHideIMESpaceEnabled) {
+          mNavBarFrameHeight = getNavigationBarFrameHeightHidden(res, mWidth > mHeight);
+        } else {
+          mNavBarFrameHeight = getNavigationBarFrameHeight(res, mWidth > mHeight);
+        }
     }
 
     /**
@@ -562,6 +570,12 @@ public class DisplayLayout {
         }
     }
 
+    public static int getNavigationBarFrameHeightHidden(Resources res, boolean landscape) {
+        return res.getDimensionPixelSize(landscape
+                ? R.dimen.navigation_bar_frame_height_landscape_hide_ime
+                : R.dimen.navigation_bar_frame_height_hide_ime);
+    }
+    
     /** @see com.android.server.wm.DisplayPolicy#getNavigationBarFrameHeight */
     public static int getNavigationBarFrameHeight(Resources res, boolean landscape) {
         return res.getDimensionPixelSize(landscape

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -63,6 +63,7 @@ import android.app.ActivityTaskManager;
 import android.app.IActivityTaskManager;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -79,6 +80,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.DeviceConfig;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
@@ -1465,6 +1467,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         int insetsHeight = -1;
         int gravity = Gravity.BOTTOM;
         boolean navBarCanMove = true;
+        boolean IsHideIMESpaceEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HIDE_IME_SPACE_ENABLE , 1, UserHandle.USER_CURRENT) != 0;
         final Context userContext = mUserContextProvider.createCurrentUserContext(mContext);
         if (mWindowManager != null && mWindowManager.getCurrentWindowMetrics() != null) {
             Rect displaySize = mWindowManager.getCurrentWindowMetrics().getBounds();
@@ -1473,8 +1477,13 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                     com.android.internal.R.bool.config_navBarCanMove);
         }
         if (!navBarCanMove) {
-            height = userContext.getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+	    if (IsHideIMESpaceEnabled) {
+              height = userContext.getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+            } else {
+              height = userContext.getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height);
+	   }
             insetsHeight = userContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.navigation_bar_height);
         } else {
@@ -1482,8 +1491,13 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 case ROTATION_UNDEFINED:
                 case Surface.ROTATION_0:
                 case Surface.ROTATION_180:
-                    height = userContext.getResources().getDimensionPixelSize(
-                            com.android.internal.R.dimen.navigation_bar_frame_height);
+                    if (IsHideIMESpaceEnabled) {
+                      height = userContext.getResources().getDimensionPixelSize(
+                              com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+                    } else {
+                      height = userContext.getResources().getDimensionPixelSize(
+                              com.android.internal.R.dimen.navigation_bar_frame_height);
+                    }
                     insetsHeight = userContext.getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
                     break;

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -33,12 +33,15 @@ import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.annotation.DrawableRes;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -1027,8 +1030,16 @@ public class NavigationBarView extends FrameLayout {
                             com.android.internal.R.dimen.navigation_bar_height_landscape)
                     : getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
-            int frameHeight = getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+	    int frameHeight;
+            boolean IsHideIMESpaceEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                     Settings.System.HIDE_IME_SPACE_ENABLE , 0, UserHandle.USER_CURRENT) != 0;
+	    if (IsHideIMESpaceEnabled) {
+              frameHeight = getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+	    } else {
+              frameHeight = getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height);	    
+	    }
             mBarTransitions.setBackgroundFrame(new Rect(0, frameHeight - height, w, h));
         } else {
             mBarTransitions.setBackgroundFrame(null);

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -432,6 +432,18 @@ public class ThemeOverlayController extends CoreStartable implements Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSystemSettings.registerContentObserverForUser(
+                Settings.System.getUriFor(Settings.System.HIDE_IME_SPACE_ENABLE),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
+
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
         mConfigurationController.addCallback(mConfigurationListener);

Colt-Enigma/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

libs/WindowManager/Shell/src/com/android/wm/shell/common/DisplayLayout.java
@@ -39,6 +39,7 @@ import android.content.res.Resources;
 import android.graphics.Insets;
 import android.graphics.Rect;
 import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Size;
@@ -88,6 +89,7 @@ public class DisplayLayout {
     private final Rect mStableInsets = new Rect();
     private boolean mHasNavigationBar = false;
     private boolean mHasStatusBar = false;
+    private boolean mIsHideIMESpaceEnabled = true;
     private int mNavBarFrameHeight = 0;
     private boolean mAllowSeamlessRotationDespiteNavBarMoving = false;
     private boolean mNavigationBarCanMove = false;
@@ -166,6 +168,8 @@ public class DisplayLayout {
         rawDisplay.getDisplayInfo(info);
         init(info, context.getResources(), hasNavigationBar(info, context, displayId),
                 hasStatusBar(displayId));
+        mIsHideIMESpaceEnabled = Settings.Secure.getIntForUser(context.getContentResolver(),
+                Settings.Secure.HIDE_IME_SPACE_ENABLE , 0, UserHandle.USER_CURRENT) != 0;
     }
 
     public DisplayLayout(DisplayLayout dl) {
@@ -224,7 +228,11 @@ public class DisplayLayout {
         if (mHasStatusBar) {
             convertNonDecorInsetsToStableInsets(res, mStableInsets, mCutout, mHasStatusBar);
         }
-        mNavBarFrameHeight = getNavigationBarFrameHeight(res, mWidth > mHeight);
+        if (mIsHideIMESpaceEnabled) {
+          mNavBarFrameHeight = getNavigationBarFrameHeightHidden(res, mWidth > mHeight);
+        } else {
+          mNavBarFrameHeight = getNavigationBarFrameHeight(res, mWidth > mHeight);
+        }
     }
 
     /**
@@ -558,6 +566,12 @@ public class DisplayLayout {
         }
     }
 
+    public static int getNavigationBarFrameHeightHidden(Resources res, boolean landscape) {
+        return res.getDimensionPixelSize(landscape
+                ? R.dimen.navigation_bar_frame_height_landscape_hide_ime
+                : R.dimen.navigation_bar_frame_height_hide_ime);
+    }
+    
     /** @see com.android.server.wm.DisplayPolicy#getNavigationBarFrameHeight */
     public static int getNavigationBarFrameHeight(Resources res, boolean landscape) {
         return res.getDimensionPixelSize(landscape

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -56,6 +56,7 @@ import android.annotation.NonNull;
 import android.app.ActivityTaskManager;
 import android.app.IActivityTaskManager;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -71,6 +72,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.DeviceConfig;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
@@ -1455,6 +1457,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         int insetsHeight = -1;
         int gravity = Gravity.BOTTOM;
         boolean navBarCanMove = true;
+        boolean IsHideIMESpaceEnabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.HIDE_IME_SPACE_ENABLE , 1, UserHandle.USER_CURRENT) != 0;
         final Context userContext = mUserContextProvider.createCurrentUserContext(mContext);
         if (mWindowManager != null && mWindowManager.getCurrentWindowMetrics() != null) {
             Rect displaySize = mWindowManager.getCurrentWindowMetrics().getBounds();
@@ -1463,8 +1467,13 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                     com.android.internal.R.bool.config_navBarCanMove);
         }
         if (!navBarCanMove) {
-            height = userContext.getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+	    if (IsHideIMESpaceEnabled) {
+              height = userContext.getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+            } else {
+              height = userContext.getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height);
+	   }
             insetsHeight = userContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.navigation_bar_height);
         } else {
@@ -1472,8 +1481,13 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 case ROTATION_UNDEFINED:
                 case Surface.ROTATION_0:
                 case Surface.ROTATION_180:
-                    height = userContext.getResources().getDimensionPixelSize(
-                            com.android.internal.R.dimen.navigation_bar_frame_height);
+                    if (IsHideIMESpaceEnabled) {
+                      height = userContext.getResources().getDimensionPixelSize(
+                              com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+                    } else {
+                      height = userContext.getResources().getDimensionPixelSize(
+                              com.android.internal.R.dimen.navigation_bar_frame_height);
+                    }
                     insetsHeight = userContext.getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
                     break;

packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -33,6 +33,7 @@ import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.annotation.DrawableRes;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Canvas;
@@ -40,6 +41,8 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
@@ -1065,8 +1068,16 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
                             com.android.internal.R.dimen.navigation_bar_height_landscape)
                     : getResources().getDimensionPixelSize(
                             com.android.internal.R.dimen.navigation_bar_height);
-            int frameHeight = getResources().getDimensionPixelSize(
-                    com.android.internal.R.dimen.navigation_bar_frame_height);
+	    int frameHeight;
+            boolean IsHideIMESpaceEnabled = Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                     Settings.Secure.HIDE_IME_SPACE_ENABLE , 0, UserHandle.USER_CURRENT) != 0;
+	    if (IsHideIMESpaceEnabled) {
+              frameHeight = getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height_hide_ime);
+	    } else {
+              frameHeight = getResources().getDimensionPixelSize(
+                      com.android.internal.R.dimen.navigation_bar_frame_height);	    
+	    }
             mBarTransitions.setBackgroundFrame(new Rect(0, frameHeight - height, w, h));
         } else {
             mBarTransitions.setBackgroundFrame(null);

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -425,6 +425,18 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             return;
         }
 
+        mSecureSettings.registerContentObserverForUser(
+                Settings.Secure.getUriFor(Settings.Secure.HIDE_IME_SPACE_ENABLE),
+                false,
+                new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Collection<Uri> collection, int flags,
+                            int userId) {
+                        reevaluateSystemTheme(true /* forceReload */);
+                    }
+                },
+                UserHandle.USER_ALL);
+
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);