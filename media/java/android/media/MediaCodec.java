9face019ae3178f82176550576863b51a5f84d32
Houxiang Dai <houxiang.dai@mediatek.com>
2023-05-10 11:53:13 +0800

    Fix some memory leakage
    Remove MediaCodec sPoll buffer leakage
    
    Test:
    DTV input source stress test pass
    
    Bug: 279135364, 235185556
    Change-Id: I751beaeba39eeabd2c6aa9059b51997130dbf86b

--- media/java/android/media/MediaCodec.java
+++ media/java/android/media/MediaCodec.java
@@ -3089,7 +3089,10 @@ final public class MediaCodec {
                 mValid = false;
                 mNativeContext = 0;
             }
-            sPool.offer(this);
+
+            if (!mInternal) {
+                sPool.offer(this);
+            }
         }
 
         private native void native_recycle();
@@ -3157,6 +3160,7 @@ final public class MediaCodec {
             mNativeContext = context;
             mMappable = isMappable;
             mValid = (context != 0);
+            mInternal = true;
         }
 
         private static final BlockingQueue<LinearBlock> sPool =
@@ -3167,6 +3171,7 @@ final public class MediaCodec {
         private boolean mMappable = false;
         private ByteBuffer mMapped = null;
         private long mNativeContext = 0;
+        private boolean mInternal = false;
     }
 
     /**
