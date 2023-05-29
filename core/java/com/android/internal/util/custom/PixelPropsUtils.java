d121f9257b3f9623c4d4e372da5d6203a873ad9f
SwapnilVicky <swapnil.motoe@gmail.com>
2023-05-29 20:11:00 +0530

    PixelPropsUtils: Add Gameprops for Battlegrounds Mobile India (BGMI)
    * Krafton disabled 90fps for android, so OP8P spoofing is no longer working
    * Now spoofing bgmi with Redmi K30 Ultra, unlocks Smooth-Extreme to HDR-Extreme
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -88,6 +88,11 @@ public class PixelPropsUtils {
             "com.gameloft.android.ANMP.GloftA9HM"
     };
 
+    private static final Map<String, Object> propsToChangeK30U;
+    private static final String[] packagesToChangeK30U = {
+            "com.pubg.imobile"
+    };
+
     private static final Map<String, Object> propsToChangeXP5;
     private static final String[] packagesToChangeXP5 = {
             "com.tencent.tmgp.kr.codm",
@@ -99,7 +104,6 @@ public class PixelPropsUtils {
     private static final String[] packagesToChangeOP8P = {
             "com.netease.lztgglobal",
             "com.tencent.ig",
-            "com.pubg.imobile",
             "com.pubg.krmobile",
             "com.vng.pubgmobile",
             "com.rekoo.pubgm",
@@ -217,6 +221,9 @@ public class PixelPropsUtils {
         propsToChangeF4 = new HashMap<>();
         propsToChangeF4.put("MODEL", "22021211RG");
         propsToChangeF4.put("MANUFACTURER", "Xiaomi");
+        propsToChangeK30U = new HashMap<>();
+        propsToChangeK30U.put("MODEL", "M2006J10C");
+        propsToChangeK30U.put("MANUFACTURER", "Xiaomi");
     }
 
     private static boolean isGoogleCameraPackage(String packageName){
@@ -275,6 +282,8 @@ public class PixelPropsUtils {
                     propsToChange.putAll(propsToChangeUserdebug);
                 }
             }
+        } else if (Arrays.asList(packagesToChangeK30U).contains(packageName)) {
+            propsToChange.putAll(propsToChangeK30U);
         } else if (Arrays.asList(packagesToChangeROG6).contains(packageName)) {
             propsToChange.putAll(propsToChangeROG6);
         } else if (Arrays.asList(packagesToChangeXP5).contains(packageName)) {
