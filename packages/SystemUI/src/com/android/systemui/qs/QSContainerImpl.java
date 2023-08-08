jimgsey/android_frameworks_base-1

SystemUI: A11 QS UI Style [1/3]
includes:
 SystemUI: Redesign Quick settings

Author: IacobIonut01 <paulionut2003@gmail.com>
Date:   Fri Apr 1 17:38:27 2022 +0000

    SystemUI: Redesign Quick settings

    - Brings up the A11 style of tiles
      + Added morph status of tiles (Circle - Active | Rounded Rect - Inactive/Disabled)
      + Kept compatibility with the secondaryLabel from A12
    - Initial support for columns/rows control in QQS, QS and Customizer
    - Removed color differencce between QSPanel and Notification Drawer
    - Aligned Notification Footer buttons with the notifications (History | Clear All)
    - Added transparency for blur support
    [Temporary removed] - Keep showing notifications when the QSPanel is fully expanded

    Bugs / W.I.P.....
    [Temporary removed] - After exiting the QS Customizer, the Notification Drawer's scrim color goes to TRANSPARENT until the user scroll slightly

    [Linux4: Remove changes made to QuickStatusBarHeader.java]
    [Linux4: Adapt for T]

    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

Author: Tim Zimmermann <tim@linux4.de>
Date:   Thu Jun 30 17:43:05 2022 +0200

    SystemUI: Use colorSurfaceHeader for QS background again

    * We want it to be pure black in dark theme, like on R

    Change-Id: I43cf9a1999601d1ce967fa79862f7153b5a24ff8

Author: SamarV-121 <samarvispute121@pm.me>
Date:   Thu Sep 29 03:34:10 2022 +0000

    base: Bring carrier info back on qs status bar header

    Change-Id: I80c18bb655bd71e01859cc72063c1aafd8fb2908

Change-Id: I9630d414cd6db3b896dcc6f0591b1cd728012f36

Author: minaripenguin <minaripenguin@users.noreply.github.com>
Date:   Wed May 3 09:18:10 2023 +0800

    SystemUI: Introduce QS UI Style settings
      * initial QS UI styles implementation using dotOS' a11 qs style

    SystemUI: Fix A11 QS UI styles compatibility with qs layout settings
      * adjust tiles configuration to fit current qs tiles settings
      * Resolve crash caused by layerdrawable -> gradi

packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -19,6 +19,7 @@ package com.android.systemui.qs;
 import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.PointF;
@@ -30,6 +31,7 @@ import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.qs.customize.QSCustomizer;
 import com.android.systemui.util.LargeScreenUtils;
+import com.android.internal.util.systemui.qs.QSLayoutUtils;
 
 import java.io.PrintWriter;
 
@@ -88,11 +90,19 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         // QSPanel will show as many rows as it can (up to TileLayout.MAX_ROWS) such that the
         // bottom and footer are inside the screen.
+        Configuration config = getResources().getConfiguration();
+        boolean navBelow = config.smallestScreenWidthDp >= 600
+                || config.orientation != Configuration.ORIENTATION_LANDSCAPE;
         MarginLayoutParams layoutParams = (MarginLayoutParams) mQSPanelContainer.getLayoutParams();
 
+        // The footer is pinned to the bottom of QSPanel (same bottoms), therefore we don't need to
+        // subtract its height. We do not care if the collapsed notifications fit in the screen.
         int availableHeight = View.MeasureSpec.getSize(heightMeasureSpec);
         int maxQs = availableHeight - layoutParams.topMargin - layoutParams.bottomMargin
                 - getPaddingBottom();
+        if (navBelow && QSLayoutUtils.getQsUiStyle(mContext) == 1) {
+            maxQs -= getResources().getDimensionPixelSize(R.dimen.navigation_bar_height);
+        }
         int padding = mPaddingLeft + mPaddingRight + layoutParams.leftMargin
                 + layoutParams.rightMargin;
         final int qsPanelWidthSpec = getChildMeasureSpec(widthMeasureSpec, padding,
@@ -100,6 +110,8 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
         mQSPanelContainer.measure(qsPanelWidthSpec,
                 MeasureSpec.makeMeasureSpec(maxQs, MeasureSpec.AT_MOST));
         int width = mQSPanelContainer.getMeasuredWidth() + padding;
+        int height = layoutParams.topMargin + layoutParams.bottomMargin
+                + mQSPanelContainer.getMeasuredHeight() + getPaddingBottom();
         super.onMeasure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(availableHeight, MeasureSpec.EXACTLY));
         // QSCustomizer will always be the height of the screen, but do this after

Colt-Enigma/platform_frameworks_base

Change-Id: I02ad5e6873527b12bf436a9b1097d5d6ce9a23bd
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -19,6 +19,7 @@ package com.android.systemui.qs;
 import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.PointF;
@@ -30,6 +31,7 @@ import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.qs.customize.QSCustomizer;
 import com.android.systemui.util.LargeScreenUtils;
+import com.android.internal.util.colt.ColtUtils.QSLayoutUtils;
 
 import java.io.PrintWriter;
 
@@ -83,11 +85,19 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         // QSPanel will show as many rows as it can (up to TileLayout.MAX_ROWS) such that the
         // bottom and footer are inside the screen.
+        Configuration config = getResources().getConfiguration();
+        boolean navBelow = config.smallestScreenWidthDp >= 600
+                || config.orientation != Configuration.ORIENTATION_LANDSCAPE;
         MarginLayoutParams layoutParams = (MarginLayoutParams) mQSPanelContainer.getLayoutParams();
 
+        // The footer is pinned to the bottom of QSPanel (same bottoms), therefore we don't need to
+        // subtract its height. We do not care if the collapsed notifications fit in the screen.
         int availableHeight = View.MeasureSpec.getSize(heightMeasureSpec);
         int maxQs = availableHeight - layoutParams.topMargin - layoutParams.bottomMargin
                 - getPaddingBottom();
+        if (navBelow && QSLayoutUtils.getQsUiStyle(mContext) == 1) {
+            maxQs -= getResources().getDimensionPixelSize(R.dimen.navigation_bar_height);
+        }
         int padding = mPaddingLeft + mPaddingRight + layoutParams.leftMargin
                 + layoutParams.rightMargin;
         final int qsPanelWidthSpec = getChildMeasureSpec(widthMeasureSpec, padding,
@@ -95,6 +105,8 @@ public class QSContainerImpl extends FrameLayout implements Dumpable {
         mQSPanelContainer.measure(qsPanelWidthSpec,
                 MeasureSpec.makeMeasureSpec(maxQs, MeasureSpec.AT_MOST));
         int width = mQSPanelContainer.getMeasuredWidth() + padding;
+        int height = layoutParams.topMargin + layoutParams.bottomMargin
+                + mQSPanelContainer.getMeasuredHeight() + getPaddingBottom();
         super.onMeasure(MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(availableHeight, MeasureSpec.EXACTLY));
         // QSCustomizer will always be the height of the screen, but do this after