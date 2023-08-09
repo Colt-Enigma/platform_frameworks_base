Spark-Rom/frameworks_base

base: Bring in Weather service API for LsWeather
This commit does not require any WeatherJaw commits in FWB or adding back weather parts.
Am Keeping Google's Weather Slice for lockscreen weather.

Special Thanks to @omnirom for the OmniJawsClient and services package
@AICP for their statusbar weather base code adapted for QS Panel and A11
Ref: https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/49630

Change-Id: Iafa109a166a6c4106b15c4bff4dfd322718bed84
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Co-authored-by: Trishiraj <gamerprince.exp@gmail.com>

packages/SystemUI/src/com/android/systemui/spark/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.internal.util.spark;
+package com.android.systemui.spark;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -41,8 +41,10 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.systemui.R;
+
 public class OmniJawsClient {
-    private static final String TAG = "OmniJawsClient";
+    private static final String TAG = "SystemUI:OmniJawsClient";
     private static final boolean DEBUG = false;
     public static final String SERVICE_PACKAGE = "org.omnirom.omnijaws";
     public static final Uri WEATHER_URI
@@ -81,8 +83,8 @@ public class OmniJawsClient {
             "setup"
     };
 
-    private static final String WEATHER_UPDATE = SERVICE_PACKAGE + ".WEATHER_UPDATE";
-    private static final String WEATHER_ERROR = SERVICE_PACKAGE + ".WEATHER_ERROR";
+    private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
+    private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
@@ -546,4 +548,3 @@ public class OmniJawsClient {
         }
     }
 }
-

packages/SystemUI/src/com/android/systemui/spark/qsweather/KGWeatherImage.java
@@ -1,0 +1,164 @@
+/*
+ * Copyright (C) 2017 AICP
+ *		 2021 CorvusOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.spark.qsweather;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.systemui.R;
+import com.android.systemui.spark.OmniJawsClient;
+
+public class KGWeatherImage extends ImageView implements
+        OmniJawsClient.OmniJawsObserver {
+
+    private String TAG = KGWeatherImage.class.getSimpleName();
+
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+
+    private int mQsWeatherEnabled;
+    private Drawable mWeatherImage;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherData;
+    private boolean mEnabled;
+    private boolean mAttached;
+
+    Handler mHandler;
+
+    public KGWeatherImage(Context context) {
+        this(context, null);
+    }
+
+    public KGWeatherImage(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public KGWeatherImage(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        mHandler = new Handler();
+        mWeatherClient = new OmniJawsClient(mContext);
+        mEnabled = mWeatherClient.isOmniJawsEnabled();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mEnabled = mWeatherClient.isOmniJawsEnabled();
+        mWeatherClient.addObserver(this);
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mWeatherClient.removeObserver(this);
+        mWeatherClient.cleanupObserver();
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.KG_SHOW_WEATHER_TEMP), false, this,
+                    UserHandle.USER_ALL);
+            updateSettings();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        if (DEBUG) Log.d(TAG, "weatherUpdated");
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        if (DEBUG) Log.d(TAG, "weatherError " + errorReason);
+    }
+
+    public void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+        mQsWeatherEnabled = Settings.System.getIntForUser(
+                resolver, Settings.System.KG_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        if ((mQsWeatherEnabled == 1 || mQsWeatherEnabled == 2 || mQsWeatherEnabled == 5)) {
+            mWeatherClient.setOmniJawsEnabled(true);
+            queryAndUpdateWeather();
+        } else {
+            setVisibility(View.GONE);
+        }
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            if (DEBUG) Log.d(TAG, "queryAndUpdateWeather " + mEnabled);
+            if (mEnabled) {
+                mWeatherClient.queryWeather();
+                mWeatherData = mWeatherClient.getWeatherInfo();
+                mWeatherImage = mWeatherClient.getWeatherConditionImage(mWeatherData.conditionCode);
+                if (mWeatherData != null) {
+                    if ((mQsWeatherEnabled == 1
+                            || mQsWeatherEnabled == 2
+                            || mQsWeatherEnabled == 5
+                            || mQsWeatherEnabled == 6)) {
+                        setImageDrawable(mWeatherImage);
+                        setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    setVisibility(View.GONE);
+                }
+            } else {
+                setVisibility(View.GONE);
+            }
+        } catch(Exception e) {
+            // Do nothing
+        }
+    }
+}

Colt-Enigma/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

packages/SystemUI/src/com/android/systemui/colt/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.internal.util.colt;
+package com.android.systemui.colt;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -41,19 +41,19 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 
+import com.android.systemui.R;
+
 public class OmniJawsClient {
-    private static final String TAG = "OmniJawsClient";
+    private static final String TAG = "SystemUI:OmniJawsClient";
     private static final boolean DEBUG = false;
     public static final String SERVICE_PACKAGE = "org.omnirom.omnijaws";
     public static final Uri WEATHER_URI
             = Uri.parse("content://org.omnirom.omnijaws.provider/weather");
     public static final Uri SETTINGS_URI
             = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
-    public static final Uri CONTROL_URI
-            = Uri.parse("content://org.omnirom.omnijaws.provider/control");
 
     private static final String ICON_PACKAGE_DEFAULT = "org.omnirom.omnijaws";
-    private static final String ICON_PREFIX_DEFAULT = "google";
+    private static final String ICON_PREFIX_DEFAULT = "outline";
     private static final String EXTRA_ERROR = "error";
     public static final int EXTRA_ERROR_NETWORK = 0;
     public static final int EXTRA_ERROR_LOCATION = 1;
@@ -83,8 +83,8 @@ public class OmniJawsClient {
             "setup"
     };
 
-    private static final String WEATHER_UPDATE = SERVICE_PACKAGE + ".WEATHER_UPDATE";
-    private static final String WEATHER_ERROR = SERVICE_PACKAGE + ".WEATHER_ERROR";
+    private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
+    private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
@@ -548,4 +548,3 @@ public class OmniJawsClient {
         }
     }
 }
-

packages/SystemUI/src/com/android/systemui/colt/qsweather/KGWeatherImage.java
@@ -1,0 +1,164 @@
+/*
+ * Copyright (C) 2017 AICP
+ *		 2021 CorvusOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.colt.qsweather;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.systemui.R;
+import com.android.systemui.colt.OmniJawsClient;
+
+public class KGWeatherImage extends ImageView implements
+        OmniJawsClient.OmniJawsObserver {
+
+    private String TAG = KGWeatherImage.class.getSimpleName();
+
+    private static final boolean DEBUG = false;
+
+    private Context mContext;
+
+    private int mQsWeatherEnabled;
+    private Drawable mWeatherImage;
+    private OmniJawsClient mWeatherClient;
+    private OmniJawsClient.WeatherInfo mWeatherData;
+    private boolean mEnabled;
+    private boolean mAttached;
+
+    Handler mHandler;
+
+    public KGWeatherImage(Context context) {
+        this(context, null);
+    }
+
+    public KGWeatherImage(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public KGWeatherImage(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        mHandler = new Handler();
+        mWeatherClient = new OmniJawsClient(mContext);
+        mEnabled = mWeatherClient.isOmniJawsEnabled();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mEnabled = mWeatherClient.isOmniJawsEnabled();
+        mWeatherClient.addObserver(this);
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mWeatherClient.removeObserver(this);
+        mWeatherClient.cleanupObserver();
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.KG_SHOW_WEATHER_TEMP), false, this,
+                    UserHandle.USER_ALL);
+            updateSettings();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    @Override
+    public void weatherUpdated() {
+        if (DEBUG) Log.d(TAG, "weatherUpdated");
+        queryAndUpdateWeather();
+    }
+
+    @Override
+    public void weatherError(int errorReason) {
+        if (DEBUG) Log.d(TAG, "weatherError " + errorReason);
+    }
+
+    public void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+        mQsWeatherEnabled = Settings.System.getIntForUser(
+                resolver, Settings.System.KG_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        if ((mQsWeatherEnabled == 1 || mQsWeatherEnabled == 2 || mQsWeatherEnabled == 5)) {
+            mWeatherClient.setOmniJawsEnabled(true);
+            queryAndUpdateWeather();
+        } else {
+            setVisibility(View.GONE);
+        }
+    }
+
+    private void queryAndUpdateWeather() {
+        try {
+            if (DEBUG) Log.d(TAG, "queryAndUpdateWeather " + mEnabled);
+            if (mEnabled) {
+                mWeatherClient.queryWeather();
+                mWeatherData = mWeatherClient.getWeatherInfo();
+                mWeatherImage = mWeatherClient.getWeatherConditionImage(mWeatherData.conditionCode);
+                if (mWeatherData != null) {
+                    if ((mQsWeatherEnabled == 1
+                            || mQsWeatherEnabled == 2
+                            || mQsWeatherEnabled == 5
+                            || mQsWeatherEnabled == 6)) {
+                        setImageDrawable(mWeatherImage);
+                        setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    setVisibility(View.GONE);
+                }
+            } else {
+                setVisibility(View.GONE);
+            }
+        } catch(Exception e) {
+            // Do nothing
+        }
+    }
+}